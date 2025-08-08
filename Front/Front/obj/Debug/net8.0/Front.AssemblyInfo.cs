9f41c210e5487ec97c770d6c1e9209c850522896
Vivian Velazquez <vivianmvero@gmail.com>
2025-08-08 17:37:49 -0600

    missing

--- Front/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
+++ Front/Front/obj/Debug/net8.0/Front.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Front")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a382f58c30ad6ee62f60f4d34384f732950d45e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883ee2ac9f5339df07e2506aa0793d2836a40b27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Front")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Front")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
