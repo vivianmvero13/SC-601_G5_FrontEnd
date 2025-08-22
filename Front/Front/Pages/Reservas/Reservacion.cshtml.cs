4119e0a053c6aa1c80e2fd2e7a432052e88320cb
Fabian-AM <fabian.alonso238@gmail.com>
2025-08-21 22:00:10 -0600

    Reservaciones Admin-Cliente
    Se agregaron las pages de Admin-Cliente

---
+++ Front/Front/Pages/Reservas/Reservacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front.Pages.Reservas
+{
+    public class ReservacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
