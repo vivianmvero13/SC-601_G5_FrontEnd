f62157b753d8b580de4d51ea853234491d2478d6
Carlos Huete <cihc2006@gmail.com>
2025-08-21 22:56:05 -0600

    Update productos
    Pagina de administracion de productos y pagina de productos del usuario para agregar al carrito

---
+++ Front/Front/Pages/Productos/Administrar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front.Pages.Productos
+{
+    public class AdministrarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
